e714c2aaeb07af76314a64a60d189ac518a1a103
Kevin <kng@ubuntu.(none)>
2014-04-15 15:00:14 -0700

    edit buildxml.php

--- buildxml.php
+++ buildxml.php
@@ -9,23 +9,36 @@
 
 $link=mysqli_connect("localhost", "root", "123", "my_db");
 // Check connection
-if (mysqli_connect_errno())
-  {
+if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error() . "<br>";
-  }
-else
-  {
+} else {
   echo "Connection successful<br>";
-  }
+}
 
 
+// adds a movie to movies table
+/*
 $sql = "INSERT INTO movies VALUES ('tt0484562', 'The Seeker: The Dark Is Rising', 'Cunningham, David L.')";
 
-if (!mysqli_query($link,$sql))
-  {
+if (!mysqli_query($link,$sql)) {
   die('Error: ' . mysqli_error($con));
-  }
+}
 echo "1 record added";
+*/
+
+
+$xml=simplexml_load_file("moviedata.xml");
+echo $xml->getName() . "<br>";
+foreach($xml->children() as $child) {
+  echo $child->getName();
+  echo "<br>";
+  echo $child->count();
+  echo "<br>";
+
+  
+}
+
+
 
 
 mysqli_close($con);
